c81286bab0768623afbca2119017d7c323865609
an <sadandboujee4474@gmail.com>
2020-10-06 16:22:09 +0700

    Change some UI

--- src/pages/Room/Footer/CardsContainer/PlayingCard/index.spec.tsx
+++ src/pages/Room/Footer/CardsContainer/PlayingCard/index.spec.tsx
@@ -3,14 +3,14 @@ import renderer from 'react-test-renderer';
 import PlayingCard from './index';
 
 describe('<PlayingCard />', () => {
-  it('should render a PlayingCard component with className="playingCard chosen custom"', () => {
+  it('should render a PlayingCard component with className="playingCard enable custom"', () => {
     const component = renderer.create(
       <PlayingCard point={3} enable={true} className="custom" />
     );
 
     const tree = component.toJSON() as renderer.ReactTestRendererJSON;
 
-    expect(tree.props.className).toBe('playingCard chosen custom');
+    expect(tree.props.className).toBe('playingCard enable custom');
   });
 
   it('should render a PlayingCard component with className="playingCard  "', () => {
