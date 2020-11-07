ef8a5cda0bcd25bacd3a2a1d3116209758f1b90d
Hoang <hoangtrinh.dev@gmail.com>
2020-11-07 23:15:53 +0700

    refactor landing page; add black color; clean up Typo

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,23 +2,20 @@ import React from 'react';
 import { Button, Typo } from '@scrpoker/components';
 import style from './style.module.scss';
 import packagejson from '../../../package.json';
-import { Link } from 'react-router-dom';
 
 const LandingPage: React.FC = () => {
   return (
     <div className={style.container}>
       <div>
         <div className={style.title}>
-          <Typo type="h2">Scrum Poker </Typo>
+          <Typo type="h1">Scrum Poker</Typo>
           <Typo type="span">v{packagejson.version}</Typo>
         </div>
         <Typo>Planing remotely has never been so easy. Why not?</Typo>
         <hr />
-        <Link to="/welcome">
-          <Button disabled={false} className={style.customButton} icon="arrow-right" type="primary">
-            Get started
-          </Button>
-        </Link>
+        <Button linkTo={'/welcome'} icon="arrow-right">
+          Get started
+        </Button>
       </div>
     </div>
   );
