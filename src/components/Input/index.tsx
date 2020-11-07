0ed10e64221d5f0bc49884131849f2e31d2c0942
Hoang <hoangtrinh.dev@gmail.com>
2020-11-08 01:24:56 +0700

    retouch Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './style.module.scss';
 
 interface Props {
-  placeholder: string;
+  placeholder?: string;
   className?: string;
   onTextChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<Props> = ({ placeholder, className = '', onTextChange }) => {
+const Input: React.FC<Props> = ({ placeholder, className, onTextChange }) => {
   return <input className={className} placeholder={placeholder} onChange={onTextChange} />;
 };
 
