b3962aaf198c6267e940cb322b65301469a472a1
Hoang <hoangtrinh.dev@gmail.com>
2020-11-07 14:52:32 +0700

    add EOF and change dev host to 127.0.0.1

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -5,6 +5,7 @@ export default merge(config, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    host: '127.0.0.1',
     historyApiFallback: true,
     clientLogLevel: 'none',
     port: 3000,
