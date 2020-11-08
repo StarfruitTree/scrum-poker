750eb6b13bf8d2af3ecc0e3dfbd1b6aad97fcc12
Hoang <hoangtrinh.dev@gmail.com>
2020-11-08 13:28:59 +0700

    add Avatar component for reusability

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Typo from './Typo';
 import Icon from './Icon';
 import Input from './Input';
 import Card from './Card';
+import AvatarInput from './AvatarInput';
 
-export { Button, Typo, Icon, Input, Card };
+export { Button, Typo, Icon, Input, Card, AvatarInput };
