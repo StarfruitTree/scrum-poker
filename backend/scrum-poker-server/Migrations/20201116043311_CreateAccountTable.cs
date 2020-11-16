998a056c4e1efc0ded3e2acb4a93dfac136232d6
an <sadandboujee4474@gmail.com>
2020-11-16 11:35:06 +0700

    Create Account table, update Room table

---
+++ backend/scrum-poker-server/Migrations/20201116043311_CreateAccountTable.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace scrum_poker_server.Migrations
+{
+    public partial class CreateAccountTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccountId",
+                table: "Rooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "varchar(255)", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_AccountId",
+                table: "Rooms",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_UserId",
+                table: "Accounts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Accounts_AccountId",
+                table: "Rooms",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Accounts_AccountId",
+                table: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_AccountId",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Rooms");
+        }
+    }
+}
---
+++ backend/scrum-poker-server/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scrum_poker_server.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection<Room> Rooms { get; set; }
+    }
+}
