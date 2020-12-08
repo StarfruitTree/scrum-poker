0000c2378e6cb49305d8dc28d3aa41ca58038c08
An Pham <anpham4474@gmail.com>
2020-12-08 12:13:28 +0700

    Refactor Login API

--- backend/scrum-poker-server/Controllers/LoginController.cs
+++ backend/scrum-poker-server/Controllers/LoginController.cs
@@ -37,10 +37,17 @@ namespace scrum_poker_server.Controllers
                 var bytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(data.Password));
                 string hashedPassword = BitConverter.ToString(bytes).Replace("-", "").ToLower();
 
-                var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == data.Email && u.Password == hashedPassword);
-                if (user == null) return Unauthorized();
+                var userRoom = await _dbContext.UserRooms.FirstOrDefaultAsync(ur => ur.User.Email == data.Email && ur.User.Password == hashedPassword);
+                if (userRoom == null) return Unauthorized();
 
-                return Ok(new { jwtToken = JwtTokenGenerator.GenerateToken(new UserData { Email = data.Email, UserId = user.Id, Name = user.Name }), expiration = 1740, userName = user.Name, userId = user.Id });
+                return Ok(new
+                {
+                    jwtToken = JwtTokenGenerator.GenerateToken(new UserData { Email = data.Email, UserId = userRoom.UserID, Name = userRoom.User.Name }),
+                    expiration = 1740,
+                    userName = userRoom.User.Name,
+                    userId = userRoom.UserID,
+                    userRoomCode = userRoom.Room.Code
+                });
             }
             else return StatusCode(422);
         }
