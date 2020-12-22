0be53e7b9a67e3b6744b07d1591c7a8cc3005fb1
An Pham <anpham4474@gmail.com>
2020-12-22 10:24:54 +0700

    Refactor UI of welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -8,14 +8,11 @@ const Welcome: React.FC = () => {
       <Card width={450}>
         <Typo type="h2">Let&apos;s get started</Typo>
         <Typo>So, you want to:</Typo>
-        <Button fullWidth linkTo="/login">
+        <Button fullWidth linkTo="/room/join">
+          Join a room
+        </Button>
+        <Button fullWidth linkTo="/login" secondary>
           Sign In
-        </Button>
-        <Button fullWidth linkTo="/signup">
-          Sign Up
-        </Button>
-        <Button fullWidth linkTo="/room/join" secondary>
-          Join a room without signing up
         </Button>
       </Card>
     </div>
