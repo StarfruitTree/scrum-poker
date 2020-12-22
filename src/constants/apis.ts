047e297587f2df896b5e809f03c9aef3cdff9dc2
Hoang <hoang@drobebox.com>
2020-12-22 09:56:42 +0700

    update production host

--- src/constants/apis.ts
+++ src/constants/apis.ts
@@ -1,7 +1,8 @@
 const getUrl = (path: string): string => `${HOST}/api/${path}`;
 const getChannel = (path: string): string => `${HOST}/${path}`;
 
-export const HOST = process.env.NODE_ENV === 'development' ? 'https://localhost:5001' : 'https://somewhere.com';
+export const HOST =
+  process.env.NODE_ENV === 'development' ? 'https://localhost:5001' : 'https://scrumpokerserver.azurewebsites.net';
 export const CREATE_ROOM = getUrl('rooms/create');
 export const JOIN_ROOM = getUrl('rooms/join');
 export const ROOM_CHANNEL = getChannel('room');
