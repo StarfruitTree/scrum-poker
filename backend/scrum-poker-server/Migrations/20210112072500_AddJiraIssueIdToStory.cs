57b5a45eb4529d72c537a3f2a3a329d18c6f3423
An Pham <anpham4474@gmail.com>
2021-01-12 17:20:20 +0700

    [BE] Add migration file

---
+++ backend/scrum-poker-server/Migrations/20210112072500_AddJiraIssueIdToStory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace scrum_poker_server.Migrations
+{
+    public partial class AddJiraIssueIdToStory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JiraIssueId",
+                table: "Stories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JiraIssueId",
+                table: "Stories");
+        }
+    }
+}
